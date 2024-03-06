23572e51aa3ea4a2d2042b41fa8f3b7fba61df03
algasami <105358814+algasami@users.noreply.github.com>
2024-03-06 18:42:09 +0800

    formatting

--- sys-firmware/src/main.cpp
+++ sys-firmware/src/main.cpp
@@ -3,26 +3,16 @@
 Settings mysettings{.discord_update_sec = 2};
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting...");
-  init_wifi();
+    Serial.begin(9600);
+    Serial.println("Starting...");
+    init_wifi();
 }
 
 void loop() {
+    if (Web::server_created) {
+        Web::server.handleClient();
+    }
 
-  if (Web::server_created) {
-    Web::server.handleClient();
-  }
-
-  handle_wifi();
-  handle_moisture();
-
-  static uint32_t last = millis();
-  uint32_t now = millis();
-  if (now - last < 1000) {
-    return;
-  } else {
-    last = now;
-    Serial.printf("Free heap: %d\n", ESP.getFreeHeap());
-  }
+    handle_wifi();
+    handle_moisture();
 }
--- sys-firmware/src/web.cpp
+++ sys-firmware/src/web.cpp
@@ -13,38 +13,38 @@ bool wifi_connected = false, server_created = false;
 using namespace Web;
 
 void init_wifi() {
-  WiFi.mode(WIFI_STA);
-  Serial.println("Begin wifi initialization");
-  for (size_t i = 0; i < ROUTER_SIZE; i++) {
-    wifi_multi.addAP(routers[i].ssid, routers[i].password);
-  }
+    WiFi.mode(WIFI_STA);
+    Serial.println("Begin wifi initialization");
+    for (size_t i = 0; i < ROUTER_SIZE; i++) {
+        wifi_multi.addAP(routers[i].ssid, routers[i].password);
+    }
 }
 
 void create_server();
 void clean_server();
 
 void handle_wifi() {
-  static uint32_t last = millis();
-  uint32_t now = millis();
-  if (now - last < 1000) {
-    return;
-  }
-  last = now;
-  if (wifi_multi.run() != WL_CONNECTED) {
-    if (wifi_connected) {
-      Serial.println("Wifi disconnected!");
-      wifi_connected = false;
-      clean_server();
+    static uint32_t last = millis();
+    uint32_t now = millis();
+    if (now - last < 1000) {
+        return;
     }
-    return;
-  }
-  if (!wifi_connected) {
-    wifi_connected = true;
-    Serial.println("Wifi connected!");
-    create_server();
-    Serial.println("Server created!");
-    server_created = true;
-  }
+    last = now;
+    if (wifi_multi.run() != WL_CONNECTED) {
+        if (wifi_connected) {
+            Serial.println("Wifi disconnected!");
+            wifi_connected = false;
+            clean_server();
+        }
+        return;
+    }
+    if (!wifi_connected) {
+        wifi_connected = true;
+        Serial.println("Wifi connected!");
+        create_server();
+        Serial.println("Server created!");
+        server_created = true;
+    }
 }
 
 /**
@@ -55,92 +55,93 @@ void handle_wifi() {
  * Caveat: the settings are not validated. We will need to add a schema for it.
  */
 void create_server() {
-  if (LittleFS.begin()) {
-    Serial.println("LittleFS mounted");
-  } else {
-    Serial.println("LittleFS failed to mount");
-    return;
-  }
-
-  server.on("/", HTTP_GET, []() {
-    Serial.println("got request");
-    if (LittleFS.exists("/index.html")) {
-      Serial.println("sending index.html");
-
-      // read /index.html into a buffer
-      File file = LittleFS.open("/index.html", "r");
-      if (file.available()) {
-        server.streamFile(file, "text/html");
-      }
-      file.close();
+    if (LittleFS.begin()) {
+        Serial.println("LittleFS mounted");
     } else {
-      Serial.println("sending 404");
-      server.send(404, "text/plain", "404: Not Found");
+        Serial.println("LittleFS failed to mount");
+        return;
     }
-  });
-  server.on("/settings", HTTP_POST, []() {
-    Serial.println("got settings");
-    // ! ugly code (I NEED TO FIX THIS!!! maybe via SCHEMA)
-    bool success = true;
-    if (server.hasArg("discord_update_sec")) {
-      int32_t og_update = server.arg("discord_update_sec").toInt();
-      if (og_update < 0) {
-        success = false;
-      } else {
-        mysettings.discord_update_sec = og_update;
-      }
+
+    server.on("/", HTTP_GET, []() {
+        Serial.println("got request");
+        if (LittleFS.exists("/index.html")) {
+            Serial.println("sending index.html");
+
+            // read /index.html into a buffer
+            File file = LittleFS.open("/index.html", "r");
+            if (file.available()) {
+                server.streamFile(file, "text/html");
+            }
+            file.close();
+        } else {
+            Serial.println("sending 404");
+            server.send(404, "text/plain", "404: Not Found");
+        }
+    });
+    server.on("/settings", HTTP_POST, []() {
+        Serial.println("got settings");
+        bool success = true;
+        if (server.hasArg("discord_update_sec")) {
+            int32_t og_update = server.arg("discord_update_sec").toInt();
+            if (og_update < 0) {
+                success = false;
+            } else {
+                mysettings.discord_update_sec = og_update;
+            }
+        }
+        if (success) {
+            server.send(201, "text/html",
+                        "<!DOCTYPE html><html><head><title>MM Update "
+                        "Success</title></head><body><h1>Success!</h1><button "
+                        "onclick=\"window.history.back()\">Go "
+                        "Back</button></body></html>");
+        } else {
+            server.send(400, "text/html",
+                        "<!DOCTYPE html><html><head><title>MM Update "
+                        "Failure</title></head><body><h1>Failure!</h1><button "
+                        "onclick=\"window.history.back()\">Go "
+                        "Back</button></body></html>");
+        }
+    });
+
+    server.on("/status", HTTP_GET, []() {
+        char buffer[50];
+        JsonDocument data;
+        data["moisture"] = moisture_value;
+        data["free_heap"] = ESP.getFreeHeap();
+        serializeJson(data, buffer);
+        server.send(200, "application/json", buffer);
+    });
+
+    server.begin();
+
+    {
+        char buffer[100];
+        sprintf(buffer,
+                "Moisture Monitor Online!\n```\nSSID:%s\nLOIP:%s\n```\n",
+                WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+        send_discord("Moisture Monitor 8266 - system", buffer);
     }
-    if (success) {
-      server.send(201, "text/html",
-                  "<!DOCTYPE html><html><head><title>MM Update "
-                  "Success</title></head><body><h1>Success!</h1><button "
-                  "onclick=\"window.history.back()\">Go "
-                  "Back</button></body></html>");
-    } else {
-      server.send(400, "text/html",
-                  "<!DOCTYPE html><html><head><title>MM Update "
-                  "Failure</title></head><body><h1>Failure!</h1><button "
-                  "onclick=\"window.history.back()\">Go "
-                  "Back</button></body></html>");
-    }
-  });
-
-  server.on("/moisture", HTTP_GET, []() {
-    char buffer[50];
-    sprintf(buffer, "%d", moisture_value);
-    server.send(200, "text/plain", buffer);
-  });
-
-  server.begin();
-
-  Serial.printf("SETUP heap size: %u\n", ESP.getFreeHeap());
-
-  {
-    char buffer[50];
-    sprintf(buffer, "Moisture Monitor Online!\n```\nSSID:%s\nLOIP:%s\n```\n",
-            WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
-    send_discord("Moisture Monitor 8266 - system", buffer);
-  }
 }
 
 void clean_server() {
-  server.close();
-  LittleFS.end();
+    server.close();
+    LittleFS.end();
 }
 
 WiFiClientSecure sec_client;
 HTTPClient http;
 void send_discord(const char *name, const char *content) {
-  char buffer[400];
-  JsonDocument data;
-  data["username"] = name;
-  data["content"] = content;
-  serializeJsonPretty(data, buffer);
-  sec_client.setInsecure();
-  http.begin(sec_client, discord_hook);
+    char buffer[500];
+    JsonDocument data;
+    data["username"] = name;
+    data["content"] = content;
+    serializeJsonPretty(data, buffer);
+    sec_client.setInsecure();
+    http.begin(sec_client, discord_hook);
 
-  http.addHeader("Content-Type", "application/json");
-  http.POST(buffer);
+    http.addHeader("Content-Type", "application/json");
+    http.POST(buffer);
 
-  http.end();
+    http.end();
 }
